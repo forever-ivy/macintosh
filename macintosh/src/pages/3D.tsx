820e302c11fe4635e88cbcc54fd2320724a52c88
forever-ivy <yezzzus@163.com>
2025-08-30 19:06:45 +0800

    remove repeated compoment

--- macintosh/src/pages/3D.tsx
+++ macintosh/src/pages/3D.tsx
@@ -16,20 +16,6 @@ export default function ScenePage() {
         dpr={[1, 1.5]}
         camera={{ position: [-25, 16, 50], fov: 35 }}
       >
-        <CameraControls
-          ref={cameraControlsRef}
-          enabled={true}
-          minDistance={5}
-          maxDistance={30}
-          infinityDolly={false}
-          dollySpeed={0.1}
-          minPolarAngle={0}
-          maxPolarAngle={Math.PI / 2}
-          smoothTime={0.25}
-          truck={false}
-          setOrbitPoint={[2.5, 0, -2.5]}
-        />
-
         <Scene
           cameraControlsRef={
             cameraControlsRef as React.RefObject<CameraControls>
